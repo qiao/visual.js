c1c5a44569706360b7b7c514258604f119513a0b
Xueqiao Xu <xueqiaoxu@gmail.com>
2012-04-18 17:48:19 +0800

    mod: default axis to <1,0,0>

--- src/objects/Primitive.js
+++ src/objects/Primitive.js
@@ -11,7 +11,7 @@ Visual.Primitive = function(scene, opts) {
   this.mesh = new THREE.Mesh(geometry, material);
 
   this.pos   = opts.pos   || new THREE.Vector3(0, 0, 0);
-  this.axis  = opts.axis  || new THREE.Vector3(0, 0, 1);
+  this.axis  = opts.axis  || new THREE.Vector3(1, 0, 0);
   this.up    = opts.up    || new THREE.Vector3(0, 1, 0);
 };
 
