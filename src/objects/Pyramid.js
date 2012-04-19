780d3f28f3ecf59c089a804f42210b4b8e1d9e67
Xueqiao Xu <xueqiaoxu@gmail.com>
2012-04-20 01:55:48 +0800

    add: pyramid

--- src/objects/Pyramid.js
+++ src/objects/Pyramid.js
@@ -1,7 +1,7 @@
 Visual.Pyramid = function(scene, opts) {
   opts = opts || {};
 
-  this._height = opts.heigth || 1;
+  this._height = opts.height || 1;
   this._width  = opts.width  || 1;
   this._length = opts.length || 1;
 
@@ -13,7 +13,43 @@ Visual.Util.inherits(Visual.Pyramid, Visual.Primitive);
 Object.defineProperties(Visual.Pyramid.prototype, {
   _buildGeometry: {
     value: function() {
+      var x = this._width / 2;
+      var y = this._height / 2;
+      var z = this._length;
+
+      // build pyramid geometry
+      // TODO: faceVertexUvs
+
       var geometry = new THREE.Geometry();
+
+      var vertices = geometry.vertices;
+      vertices.push(new THREE.Vertex(new THREE.Vector3(x, y, 0)));
+      vertices.push(new THREE.Vertex(new THREE.Vector3(x, -y, 0)));
+      vertices.push(new THREE.Vertex(new THREE.Vector3(-x, -y, 0)));
+      vertices.push(new THREE.Vertex(new THREE.Vector3(-x, y, 0)));
+      vertices.push(new THREE.Vertex(new THREE.Vector3(0, 0, z)));
+
+      var faces = geometry.faces;
+      faces.push(new THREE.Face4(0, 1, 2, 3));
+      faces.push(new THREE.Face3(0, 4, 1)) ;
+      faces.push(new THREE.Face3(1, 4, 2));
+      faces.push(new THREE.Face3(2, 4, 3));
+      faces.push(new THREE.Face3(3, 4, 0));
+
+      geometry.computeCentroids();
+      geometry.computeFaceNormals();
+
+      // bottom's vertex normals
+      var n = new THREE.Vector3(0, 0, -1);
+      faces[0].vertexNormals = [n, n, n, n];
+
+      // sides' vertex normals
+      for (var i = 0; i < 4; ++i) {
+        var face = faces[i];
+        n = face.normal;
+        face.vertexNormals = [n, n, n];
+      }
+
       return geometry;
     }
   },
